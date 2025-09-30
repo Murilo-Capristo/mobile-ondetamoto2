12555b672f74c9f3c9c95e326b1aa73877fc5643
Murilo-Capristo <murilocapristo@gmail.com>
2025-09-30 19:53:18 -0300

    (fix): exclue userContext

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootNavigator from './src/navigation/RootNavigator';
 import { useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { AuthProvider } from './src/screens/contexts/UserContext';
 import { useNavigation } from '@react-navigation/native';
 
 export default function App() {
@@ -20,10 +19,8 @@ export default function App() {
   }, []);
 
   return (
-    <AuthProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <RootNavigator />
       </GestureHandlerRootView>
-    </AuthProvider>
   );
 }
