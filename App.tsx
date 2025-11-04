0db38c48c7eec4cb38224c7d238931be24f7d481
Murilo-Capristo <murilocapristo@gmail.com>
2025-11-04 16:28:45 -0300

    fix(useContext): breaking importing cicle to resolve the error 'runtime Not ready'

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import RootNavigator from './src/navigation/RootNavigator';
+// NÃO IMPORTE O RootNavigator AQUI EM CIMA
+// import RootNavigator from './src/navigation/RootNavigator'; 
 import { PaperProvider } from 'react-native-paper';
 import { ThemeProvider, useThemeContext } from './src/context/ThemeContext';
 import { AuthProvider } from './src/context/UserContext';
@@ -17,6 +18,12 @@ export default function App() {
 
 function MainApp() {
   const { theme } = useThemeContext();
+
+  // IMPORTE O RootNavigator AQUI DENTRO
+  // Usamos 'require' para fazer a importação de forma atrasada (lazy).
+  // Isso garante que o UserContext já foi carregado antes do RootNavigator.
+  // Correção do erro [runtime Not ready]: TypeError: Cannot read property 'useContext' of null, js engine: hermes, stack:
+  const RootNavigator = require('./src/navigation/RootNavigator').default;
 
   return (
     <PaperProvider theme={theme}>
